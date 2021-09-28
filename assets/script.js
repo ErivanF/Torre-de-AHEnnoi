2145d246e7da49e7d71ab55c9a42a68e4c444ab5
MateusVSousa <min705555@gmail.com>
2021-09-28 14:56:16 -0300

    Discos em DOM

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
-const pino1 = document.getElementById("pino1")
-const pino2 = document.getElementById("pino2")
-const pino3 = document.getElementById("pino3")
+const pino1 = document.getElementById("pino1");
+const pino2 = document.getElementById("pino2");
+const pino3 = document.getElementById("pino3");
 //Mateus
+let disco1 = document.createElement("div");
+disco1.id = "disco1" ;
+pino1.appendChild(disco1);
+const disco2 = document.createElement("div")
+disco2.id = "disco2" ;
+pino1.appendChild(disco2);
+const disco3 = document.createElement("div")
+disco3.id = "disco3" ;
+pino1.appendChild(disco3);
+const disco4 = document.createElement("div")
+disco4.id = "disco4" ;
+pino1.appendChild(disco4);
+const disco5 = document.createElement("div")
+disco5.id = "disco5" ;
+pino1.appendChild(disco5);
 
 
 //Erivan
